cc967d86e3822c195264dae8c26bf14bbb67ff16
Grace Zhang <gz.gracez16@gmail.com>
2017-01-12 11:12:51 -0500

    getAccounts, getTotalBalance per customer

--- Alexa Skill/helpers.py
+++ Alexa Skill/helpers.py
@@ -3,15 +3,17 @@ import json
 
 apiKey = "638e3a40768577cc14440e93f78f7085"
 
-def getAccounts():
-	accountsUrl = 'http://api.reimaginebanking.com/accounts?key={}'.format(apiKey)
+def getAccounts(customerID):
+	accountsUrl = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerID, apiKey)
 	accountsResponse = requests.get(accountsUrl)
 	if accountsResponse.status_code == 200:
 		accounts = json.loads(accountsResponse.text)
-	return accounts
+		return accounts
+	else:
+		return None
 
-def getTotalBalance():
-	accounts = getAccounts()
+def getTotalBalance(customerID):
+	accounts = getAccounts(customerID)
 	current = 0
 	for i in accounts:
 		if i["type"].lower() == "credit card":
@@ -22,5 +24,13 @@ def getTotalBalance():
 			continue
 	return current
 
+def getPurchases(customerID):
+	accounts = getAccounts(customerID)
+	for i in accounts:
+		accountID = i["_id"]
+		purchasesUrl = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(accountID, apiKey)
+		purchasesResponse = requests.get(purchasesUrl)
+		print json.dumps(i)
+
 if __name__=="__main__":
-    print getTotalBalance()
+    print getPurchases("58000d58360f81f104543d82")
