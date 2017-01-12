8b9c7b6b20686f9f79a06d1174aae57a60d637ac
Grace Zhang <gz.gracez16@gmail.com>
2017-01-12 10:58:58 -0500

    getTotalBalance

--- Alexa Skill/helpers.py
+++ Alexa Skill/helpers.py
@@ -3,12 +3,24 @@ import json
 
 apiKey = "638e3a40768577cc14440e93f78f7085"
 
-def getAccountBalance():
+def getAccounts():
 	accountsUrl = 'http://api.reimaginebanking.com/accounts?key={}'.format(apiKey)
 	accountsResponse = requests.get(accountsUrl)
 	if accountsResponse.status_code == 200:
 		accounts = json.loads(accountsResponse.text)
 	return accounts
 
+def getTotalBalance():
+	accounts = getAccounts()
+	current = 0
+	for i in accounts:
+		if i["type"].lower() == "credit card":
+			current -= i["balance"]
+		elif i["type"].lower() == "checking":
+			current += i["balance"]
+		else:
+			continue
+	return current
+
 if __name__=="__main__":
-    print getAccountBalance()
+    print getTotalBalance()
