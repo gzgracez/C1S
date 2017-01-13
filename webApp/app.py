95704da3e816cf6b3d90a482c1cbdd6f74285d89
Jaymo Kang <jaymokang@gmail.com>
2017-01-12 23:50:36 -0500

    Form to update allocations

--- webApp/app.py
+++ webApp/app.py
@@ -5,7 +5,7 @@ import sys
 import os
 import datetime
 sys.path.append('../Alexa')
-from helpers import getAccounts, getAccountAndBalance, getCheckingBalance, getTotalBalance, getPurchases,getTotalforDOW, calculateSuggestedToday, getAllocations
+from helpers import getAccounts, getAccountAndBalance, getCheckingBalance, getTotalBalance, getPurchases,getTotalforDOW, calculateSuggestedToday, getAllocations, addAllocation, updateAllocations
 # import ../AlexaSkill/helpers.py
 
 app = Flask(__name__)
@@ -44,9 +44,9 @@ def home():
             account = result[key]
             ab.append({"type": account[0], "balance": "${:,.2f}".format(account[1])})
         yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-        return render_template("index.html", checkingTotal = "${:,.2f}".format(getCheckingBalance(customerID)),
-        totalBalance = "${:,.2f}".format(getTotalBalance(customerID)),
-        targetToday = "${:,.2f}".format(calculateSuggestedToday(customerID, datetime.datetime.now().weekday())))
+        return render_template("index.html")#, checkingTotal = "${:,.2f}".format(getCheckingBalance(customerID)),
+        #totalBalance = "${:,.2f}".format(getTotalBalance(customerID)),
+        #targetToday = "${:,.2s}".format(calculateSuggestedToday(customerID, datetime.datetime.now().weekday())))
 
 @app.route('/login', methods=["GET", 'POST'])
 def login():
@@ -85,20 +85,24 @@ def purchases():
     for key in result:
         purchase = result[key]
         d.append({"merchantID": purchase[3], "purchaseDate": purchase[1], "amount": "${:,.2f}".format(purchase[2])})
-        d = sorted(d, key=lambda k: datetime.datetime.strptime(k['purchaseDate'],"%Y-%m-%d"), reverse=True)
+    d = sorted(d, key=lambda k: datetime.datetime.strptime(k['purchaseDate'],"%Y-%m-%d"), reverse=True)
     return render_template("purchases.html", purchases=d)
 
-@app.route("/allocations")
+@app.route("/allocations", methods=["GET", "POST"])
 def allocations():
+    if request.method == "POST":
+        addAllocation(customerID, request.form["category"], request.form["amount"], request.form["date"])
+    updateAllocations(customerID)
     data = getAllocations(customerID)
     print(str(data))
     l = []
     total = 0
     for dat in data:
-        l.append({"cat": dat[0], "amount": str(dat[1])})
+        l.append({"cat": dat[0], "amount": "${:,.2f}".format(dat[1]), "date": dat[3]})
         total += dat[1]
+    l = sorted(l, key=lambda k: datetime.datetime.strftime(k['date'],"%Y-%m-%d"), reverse=True)
     print(l)
-    return render_template("allocations.html", allocations=l, total=total)
+    return render_template("allocations.html", allocations=l, total="${:,.2f}".format(total))
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
